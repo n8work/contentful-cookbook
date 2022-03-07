93be851f9a94a7f413b0d5214bdaa150295a8873
ChrisH <98359604+n8work@users.noreply.github.com>
2022-03-07 17:47:50 +0100

    basic contentful setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+const contentful = require('contentful');
+
+const client = contentful.createClient({
+  space: process.env.REACT_APP_contentful_space,
+  accessToken: process.env.REACT_APP_contentful_token
+});
+
+const content = document.querySelector("#content");
+
+client.getEntries().then(function (entries) {
+
+  // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/entries/entry
+
+  entries.items.forEach(function (entry) {
+
+    console.log("Entries OBJ: ", entry);
+    console.log("Entries Title: ", entry.fields.title);
+    console.log("Entries Author: ", entry.fields.author);
+    console.log("Locale: ", entry.sys.locale);
+
+  });
+});
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <p id="content"></p>
     </div>
   );
 }
