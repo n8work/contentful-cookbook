ab9d2e0d794b8932c09e146542b4d5d98c14624e
Olin Kirkland <me@olinkirk.land>
2022-03-14 16:54:02 +0100

    Add & style gallery

--- src/model/Recipe.js
+++ src/model/Recipe.js
@@ -1,11 +1,16 @@
 export default class Recipe {
-  constructor(id, title, description, image, author, date) {
+  constructor(id, title, description, image, author, date, gallery) {
     this.id = id;
     this.title = title; // string
     this.description = description; // [string, string, ...]
     this.image = image; // string
     this.author = author; // {id:string, name:{first:string, last:string}, image:string}
     this.date = date; // Date
+    this.gallery = gallery; //
+  }
+
+  get slug() {
+    return this.title.replaceAll(' ', '-').toLowerCase();
   }
 
   render() {
@@ -17,18 +22,24 @@ export default class Recipe {
         </p>
 
         <div className="recipe-step-heroimage">
-            <img src={this.image} alt={this.title} />
+          <img src={this.image} alt={this.title} />
         </div>
 
         <ul className="recipe-step-list">
           {this.description.map((d, index) => (
-            <li className="recipe-step">
+            <li key={index} className="recipe-step">
               <div className="recipe-step-number-box">
                 <p className="recipe-step-number">{index + 1}</p>
               </div>
-              <p key={Math.random()} className="recipe-step-body">
-                {d}
-              </p>
+              <p className="recipe-step-body">{d}</p>
+            </li>
+          ))}
+        </ul>
+
+        <ul className="gallery">
+          {this.gallery.map((g, index) => (
+            <li key={index} className="gallery-item">
+              <img src={`${g}`} alt="" />
             </li>
           ))}
         </ul>
@@ -37,6 +48,8 @@ export default class Recipe {
   }
 
   static fromUntyped(data) {
+    // console.log(data);
+
     const r = data.fields;
 
     // Id
@@ -65,7 +78,21 @@ export default class Recipe {
     // Date
     const date = Date.parse(data.sys.createdAt);
 
-    let recipe = new Recipe(id, title, description, image, author, date);
+    // Gallery
+    const gallery = r.RecipeImageGallery
+      ? r.RecipeImageGallery.map((g) => g.fields.file.url)
+      : [];
+
+    let recipe = new Recipe(
+      id,
+      title,
+      description,
+      image,
+      author,
+      date,
+      gallery
+    );
+
     console.log('======== RECIPE ========');
     console.log(recipe);
     return recipe;
