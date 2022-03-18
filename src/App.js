f4c8b6dbbedfdd0a49b1bc135fb9e0076412385c
ChrisH <98359604+n8work@users.noreply.github.com>
2022-03-18 02:53:54 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import RecipeComponent from './components/RecipeComponent';
 import User from './components/User';
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,67 @@
-import * as React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import * as React from "react";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function Header() {
+  const { i18n } = useTranslation();
+
+  const setLangEN = () => {
+    console.log("setLang -> EN");
+    i18n.changeLanguage("en");
+  };
+
+  const setLangDE = () => {
+    console.log("setLang -> DE");
+    i18n.changeLanguage("de");
+  };
+
+  const setLangFR = () => {
+    console.log("setLang -> FR");
+    i18n.changeLanguage("fr");
+  };
+
+  let activeCn = ".navlink-lang active";
+  let deactiveCn = ".navlink-lang";
+
   return (
     <>
-    
-    <nav>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/imprint">Imprint</NavLink>
-    </nav>
+      <nav>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/imprint">Imprint</NavLink>
+      </nav>
 
-        <div className="languageSwitcher">
-          <button onClick={setLangEN} className="buttonLang">English</button>
-          <button onClick={setLangDE} className="buttonLang">Deutsch</button>
-          <button onClick={setLangFR} className="buttonLang">Français</button>
+      <div className="language-switcher">
+        <button onClick={setLangEN} className="button-lang">
+          English
+        </button>
+        <button onClick={setLangDE} className="button-lang">
+          Deutsch
+        </button>
+        <button onClick={setLangFR} className="button-lang">
+          Français
+        </button>
 
-          {/* */}
-          <NavLink to="/en/" className={ ({isActive}) => isActive ? activeCn : deactiveCn }>🇺🇸</NavLink>
-          <NavLink to="/de/" className={ ({isActive}) => isActive ? activeCn : deactiveCn }>🇩🇪</NavLink>
-          <NavLink to="/fr/" className={ ({isActive}) => isActive ? activeCn : deactiveCn }>🇫🇷</NavLink> 
-          {/* */}
-        </div>
-
+        {/* */}
+        <NavLink
+          to="/en/"
+          className={({ isActive }) => (isActive ? activeCn : deactiveCn)}
+        >
+          🇺🇸
+        </NavLink>
+        <NavLink
+          to="/de/"
+          className={({ isActive }) => (isActive ? activeCn : deactiveCn)}
+        >
+          🇩🇪
+        </NavLink>
+        <NavLink
+          to="/fr/"
+          className={({ isActive }) => (isActive ? activeCn : deactiveCn)}
+        >
+          🇫🇷
+        </NavLink>
+        {/* */}
+      </div>
     </>
   );
 
