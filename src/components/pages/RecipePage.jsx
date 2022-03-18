5a55a4f49c6738e9548c671089e6178650c5528f
Olin Kirkland <me@olinkirk.land>
2022-03-18 16:39:21 +0100

    Get routing to work! Finally!

--- src/components/pages/RecipePage.jsx
+++ src/components/pages/RecipePage.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Recipe from '../../model/Recipe';
+import RecipeComponent from '../RecipeComponent';
+var contentful = require('contentful');
+
 export default function RecipePage() {
+  const { id } = useParams();
+
+  const [recipe, setRecipe] = useState();
+
+  const client = contentful.createClient({
+    space: process.env.REACT_APP_contentful_space,
+    accessToken: process.env.REACT_APP_contentful_token
+  });
+
+  useEffect(() => {
+    const query = { content_type: 'wbsRecipe', 'fields.slug': id };
+    client.getEntries(query).then(function (untypedRecipes) {
+      setRecipe(Recipe.fromUntyped(untypedRecipes.items[0]));
+    });
+  }, []);
 
   return (
-    <article className="recipePage">
-       Recipe Page
+    <article className="page recipe">
+      <p>Recipe Page: {id}</p>
+      <RecipeComponent recipe={recipe} />
     </article>
-  )
-
+  );
 }
